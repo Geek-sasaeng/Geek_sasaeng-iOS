9e72592c23a322341a934fd6176f5ccc619b6663
EunsuSeo01 <eunsu0613@naver.com>
2022-07-01 19:35:12 +0900

    Feat: 회원가입 화면 구현

--- Geeksasaeng/Geeksasaeng/SceneDelegate.swift
+++ Geeksasaeng/Geeksasaeng/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     guard let windowScene = scene as? UIWindowScene else { return }
     self.window = UIWindow(windowScene: windowScene)
     
-    let rootViewController = ViewController()
+    let rootViewController = RegisterViewController()
     
     self.window?.rootViewController = rootViewController
     self.window?.makeKeyAndVisible()
---
+++ Geeksasaeng/Geeksasaeng/UIFont+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  UIFont+Extension.swift
+//  Geeksasaeng
+//
+//  Created by 서은수 on 2022/07/01.
+//
+// extension을 활용하여 swift 코드 내에서 편리하게 폰트를 적용 가능!
+// - ex. label.font = .customFont(.neoBold, size: 16)
+
+import UIKit
+
+extension UIFont {
+    public enum customFontType: String {
+        case gmarketSansMedium = "GmarketSansMedium"
+        case neoBold = "SpoqaHanSansNeo-Bold"
+        case neoLight = "SpoqaHanSansNeo-Light"
+        case neoMedium = "SpoqaHanSansNeo-Medium"
+        case neoRegular = "SpoqaHanSansNeo-Regular"
+    }
+
+    static func customFont(_ type: customFontType, size: CGFloat) -> UIFont {
+        return UIFont(name: "\(type.rawValue)", size: size)!
+    }
+}
