49462a4ff970b198f4f49e094b12f0323f55e2b0
EunsuSeo01 <eunsu0613@naver.com>
2022-07-02 01:32:55 +0900

    Chore: DeliveryVC 에러나서 재생성

--- Geeksasaeng/Geeksasaeng/ViewController/DeliveryVC.swift
+++ Geeksasaeng/Geeksasaeng/ViewController/DeliveryVC.swift
@@ -2,10 +2,12 @@
 //  DeliveryVC.swift
 //  Geeksasaeng
 //
-//  Created by 조동진 on 2022/07/02.
+//  Created by 서은수 on 2022/07/02.
 //
 
 import UIKit
 import SnapKit
-import Kingfisher
-import Alamofire
+
+class DeliveryViewController: UIViewController {
+    
+}
