64bc0c0ff93d2f5e40475a4af8ccf86665f3dee1
Jodongjin <ehdwlsdlwkd@naver.com>
2022-07-02 21:40:46 +0900

    Feat: 버튼 토글 구현

--- Geeksasaeng/Geeksasaeng/ViewController/LoginVC.swift
+++ Geeksasaeng/Geeksasaeng/ViewController/LoginVC.swift
@@ -9,21 +9,21 @@ import UIKit
 import SnapKit
 
 class LoginViewController: UIViewController {
-  
-  // MARK: Variables
+  // MARK: Components
   let logoImageView: UIImageView = {
     let imageView = UIImageView(image: UIImage(imageLiteralResourceName: "AppLogo"))
     return imageView
   }()
-
+  
   var idTextField: UITextField = {
     var textField = UITextField()
     textField.font = .customFont(.neoLight, size: 14)
     textField.attributedPlaceholder = NSAttributedString(
-        string: "아이디",
-        attributes: [NSAttributedString.Key.foregroundColor: UIColor.init(hex: 0xD8D8D8)]
+      string: "아이디",
+      attributes: [NSAttributedString.Key.foregroundColor: UIColor.init(hex: 0xD8D8D8)]
     )
     textField.makeBottomLine(335)
+    textField.addTarget(self, action: #selector(didChangeTextField(_:)), for: .editingChanged)
     return textField
   }()
   
@@ -31,23 +31,25 @@ class LoginViewController: UIViewController {
     var textField = UITextField()
     textField.font = .customFont(.neoLight, size: 14)
     textField.attributedPlaceholder = NSAttributedString(
-        string: "비밀번호",
-        attributes: [NSAttributedString.Key.foregroundColor: UIColor.init(hex: 0xD8D8D8)]
+      string: "비밀번호",
+      attributes: [NSAttributedString.Key.foregroundColor: UIColor.init(hex: 0xD8D8D8)]
     )
     textField.makeBottomLine(335)
+    textField.addTarget(self, action: #selector(didChangeTextField(_:)), for: .editingChanged)
     return textField
   }()
   
   let loginButton: UIButton = {
     let button = UIButton()
     button.setTitle("로그인", for: .normal)
-    button.setTitleColor(UIColor.black, for: .normal)
+    //    button.setTitleColor(UIColor.black, for: .normal)
     button.titleLabel?.font = .customFont(.neoBold, size: 20)
-    button.titleLabel?.textColor = .white
-    button.backgroundColor = UIColor.init(hex: 0x29ABE2)
+    button.titleLabel?.textColor = UIColor(hex: 0xA8A8A8)
+    button.backgroundColor = UIColor.init(hex: 0xEFEFEF)
     button.setTitleColor(UIColor.white, for: .normal)
     button.layer.cornerRadius = 5
-      button.addTarget(self, action: #selector(tapLoginButton), for: .touchUpInside)
+    button.isEnabled = false
+    button.addTarget(self, action: #selector(tapLoginButton), for: .touchUpInside)
     return button
   }()
   
@@ -90,7 +92,7 @@ class LoginViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     view.backgroundColor = .white
-
+    
     addSubViews()
     configLayouts()
   }
@@ -140,53 +142,83 @@ class LoginViewController: UIViewController {
       make.top.equalTo(self.loginButton.snp.bottom).offset(10)
       make.height.equalTo(51)
     }
-
+    
     automaticLoginButton.snp.makeConstraints { make in
       make.centerX.equalTo(self.logoImageView)
       make.top.equalTo(self.naverLoginButton.snp.bottom).offset(15)
     }
-
+    
     signUpButton.snp.makeConstraints { make in
       make.centerX.equalTo(self.logoImageView)
       make.top.equalTo(self.automaticLoginButton.snp.bottom).offset(45)
     }
   }
+  
+  
+  // MARK: Methods
+  @objc func showRegisterView() {
+    // registerVC로 화면 전환.
+    let registerVC = RegisterViewController()
     
-    @objc func showRegisterView() {
-        // registerVC로 화면 전환.
-        let registerVC = RegisterViewController()
-        
-        registerVC.modalTransitionStyle = .coverVertical
-        registerVC.modalPresentationStyle = .fullScreen
-        
-        present(registerVC, animated: true)
+    registerVC.modalTransitionStyle = .coverVertical
+    registerVC.modalPresentationStyle = .fullScreen
+    
+    present(registerVC, animated: true)
+  }
+  
+  @objc func tapLoginButton() {
+    // 로그인 시도
+    if let id = self.idTextField.text,
+       let pw = self.passwordTextField.text {
+      let input = LoginInput(loginId: id, password: pw)
+      LoginManager.login(self, input)
     }
-   
-    @objc func tapLoginButton() {
-        // 로그인 시도
-        if let id = self.idTextField.text,
-           let pw = self.passwordTextField.text {
-            let input = LoginInput(loginId: id, password: pw)
-            LoginManager.login(self, input)
-        }
+  }
+  
+  @objc func didChangeTextField(_ sender: UITextField) {
+    let text = sender.text ?? ""
+    
+    switch sender {
+    case idTextField:
+      if text.count >= 1 && passwordTextField.text?.count ?? 0 >= 1 {
+        loginButton.isEnabled = true
+        loginButton.tintColor = .white
+        loginButton.backgroundColor = .mainColor
+      } else {
+        loginButton.isEnabled = false
+        loginButton.tintColor = UIColor(hex: 0xA8A8A8)
+        loginButton.backgroundColor = UIColor(hex: 0xEFEFEF)
+      }
+    default:
+      if text.count >= 1 && idTextField.text?.count ?? 0 >= 1 {
+        loginButton.isEnabled = true
+        loginButton.tintColor = .white
+        loginButton.backgroundColor = .mainColor
+      } else {
+        loginButton.isEnabled = false
+        loginButton.tintColor = UIColor(hex: 0xA8A8A8)
+        loginButton.backgroundColor = UIColor(hex: 0xEFEFEF)
+      }
     }
     
-    public func showHomeView() {
-        let tabBarController = TabBarController()
-        tabBarController.modalTransitionStyle = .crossDissolve
-        tabBarController.modalPresentationStyle = .fullScreen
-        present(tabBarController, animated: true)
-    }
+  }
+  
+  public func showHomeView() {
+    let tabBarController = TabBarController()
+    tabBarController.modalTransitionStyle = .crossDissolve
+    tabBarController.modalPresentationStyle = .fullScreen
+    present(tabBarController, animated: true)
+  }
 }
 
 
 // Extensions
 extension UIView {
-
+  
   func addBottomBorderWithColor(color: UIColor, width: CGFloat) {
-      let border = CALayer()
-      border.backgroundColor = color.cgColor
-      border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: width)
-      self.layer.addSublayer(border)
+    let border = CALayer()
+    border.backgroundColor = color.cgColor
+    border.frame = CGRect(x: 0, y: self.frame.size.height - width, width: self.frame.size.width, height: width)
+    self.layer.addSublayer(border)
   }
 }
