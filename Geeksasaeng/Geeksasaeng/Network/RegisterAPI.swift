34347764492f4edb06283d2709556a4992cfceab
Jodongjin <ehdwlsdlwkd@naver.com>
2022-07-08 00:43:52 +0900

    Refactor: Network 폴더 생성, RegisterAPI 추가

--- Geeksasaeng/Geeksasaeng/Network/RegisterAPI.swift
+++ Geeksasaeng/Geeksasaeng/Network/RegisterAPI.swift
@@ -1,14 +1,42 @@
 //
-//  RegisterManager.swift
+//  RegisterModel.swift
 //  Geeksasaeng
 //
 //  Created by 서은수 on 2022/07/02.
 //
 
+import Foundation
 import Alamofire
 
+// 회원가입을 요청했을 때 받게 될 Response의 형태.
+struct RegisterModel : Decodable {
+    var isSuccess : Bool?
+    var code : Int?
+    var message : String?
+    var result : RegisterModelResult?
+}
+
+struct RegisterModelResult : Decodable {
+    var loginId: String?
+    var nickname: String?
+    var universityName: String?
+    var email: String?
+    var phoneNumber: String?
+}
+
+// 회원가입을 완료했을 때 보낼 Request body의 형태.
+struct RegisterInput : Encodable {
+    var checkPassword: String?
+    var email: String?
+    var loginId: String?
+    var nickname: String?
+    var password: String?
+    var phoneNumber: String?
+    var universityName: String?
+}
+
 // 회원가입 API 연동
-class RegisterViewModel {
+class RegisterAPI {
     public static func registerUser(_ viewController : EmailAuthViewController, _ parameter : RegisterInput) {
         AF.request("https://geeksasaeng.shop/members", method: .post,
                    parameters: parameter, encoder: JSONParameterEncoder.default, headers: nil)
