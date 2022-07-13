a42b3d134e648575739236e18510e427749398e5
Jodongjin <ehdwlsdlwkd@naver.com>
2022-07-13 22:49:43 +0900

    Feat: 카카오맵 라이브러리 연결

--- Geeksasaeng/Geeksasaeng/View/ViewController/CreatePartySubVC/ReceiptPlaceVC.swift
+++ Geeksasaeng/Geeksasaeng/View/ViewController/CreatePartySubVC/ReceiptPlaceVC.swift
@@ -26,7 +26,15 @@ class ReceiptPlaceViewController: UIViewController {
         return button
     }()
     
-    /* nextButton: 다음 버튼 */
+    let mapSubView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .gray
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 5
+        return view
+    }()
+    
+    /* confirmButton: 완료 버튼 */
     let confirmButton: UIButton = {
         let button = UIButton()
         button.setTitle("완료", for: .normal)
@@ -39,6 +47,9 @@ class ReceiptPlaceViewController: UIViewController {
         button.addTarget(self, action: #selector(removeAllSubVC), for: .touchUpInside)
         return button
     }()
+    
+    // MARK: - Properties
+    var mapView: MTMapView?
     
     /* pageLabel: 4/4 */
     let pageLabel: UILabel = {
@@ -54,12 +65,31 @@ class ReceiptPlaceViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         
+        setMapView()
         setViewLayout()
         setSubViews()
         setLayouts()
     }
     
     // MARK: - Functions
+    private func setMapView() {
+        // 지도 불러오기
+        mapView = MTMapView(frame: mapSubView.frame)
+        if let mapView = mapView {
+            // 델리게이트 연결
+            mapView.delegate = self
+            // 지도의 타입 설정 - hybrid: 하이브리드, satellite: 위성지도, standard: 기본지도
+            mapView.baseMapType = .standard
+            
+            // 현재 위치 트래킹
+            mapView.currentLocationTrackingMode = .onWithoutHeading
+            mapView.showCurrentLocationMarker = true
+            
+            // 지도의 센터를 설정 (x와 y 좌표, 줌 레벨 등)
+            mapView.setMapCenter(MTMapPoint(geoCoord: MTMapPointGeo(latitude: 37.456518177069526, longitude: 126.70531256589555)), zoomLevel: 5, animated: true)
+        }
+    }
+    
     private func setViewLayout() {
         view.layer.masksToBounds = true
         view.layer.cornerRadius = 7
@@ -69,8 +99,15 @@ class ReceiptPlaceViewController: UIViewController {
         }
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        // mapView의 모든 poiItem 제거
+        for item in mapView!.poiItems {
+            mapView?.remove(item as! MTMapPOIItem)
+        }
+    }
+    
     private func setSubViews() {
-        [titleLabel, backButton, confirmButton, pageLabel].forEach {
+        [titleLabel, backButton, mapSubView, confirmButton, pageLabel].forEach {
             view.addSubview($0)
         }
     }
@@ -86,6 +123,13 @@ class ReceiptPlaceViewController: UIViewController {
             make.left.equalToSuperview().inset(31)
         }
         
+        mapSubView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(106)
+            make.centerX.equalToSuperview()
+            make.width.equalTo(262)
+            make.height.equalTo(144)
+        }
+        
         confirmButton.snp.makeConstraints { make in
             make.width.equalTo(262)
             make.height.equalTo(51)
@@ -99,7 +143,44 @@ class ReceiptPlaceViewController: UIViewController {
         }
     }
     
+    /*
+    // 마커 추가
+    private func makeMarker() {
+        /*
+        저는 서버 api를 통해 가져온 데이터를 resultList에 담았어요
+        이름, 좌표, 주소 등을 담은 구조체를 담은 배열이에요
+        */
+    
+        // cnt로 마커의 tag를 구분
+        var cnt = 0
+        for item in resultList {
+            self.mapPoint1 = MTMapPoint(geoCoord: MTMapPointGeo(latitude: item.x, longitude: item.y
+            ))
+            poiItem1 = MTMapPOIItem()
+            // 핀 색상 설정
+            poiItem1?.markerType = MTMapPOIItemMarkerType.redPin
+            poiItem1?.mapPoint = mapPoint1
+            // 핀 이름 설정
+            poiItem1?.itemName = item.placeName
+            // 태그 설정
+            poiItem1?.tag = cnt
+            // 맵뷰에 추가!
+            mapView!.add(poiItem1)
+            cnt += 1
+        }
+    }
+     */
+    
     @objc func removeAllSubVC() {
         NotificationCenter.default.post(name: NSNotification.Name("TapConfirmButton"), object: "true")
     }
+}
+
+extension ReceiptPlaceViewController: MTMapViewDelegate {
+    // poiItem 클릭 이벤트
+    func mapView(_ mapView: MTMapView!, touchedCalloutBalloonOf poiItem: MTMapPOIItem!) {
+        // 인덱스는 poiItem의 태그로 접근
+        let index = poiItem.tag
+    }
+    
 }
