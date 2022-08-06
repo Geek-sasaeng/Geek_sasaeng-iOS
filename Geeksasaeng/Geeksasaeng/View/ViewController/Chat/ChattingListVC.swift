6035eced66afabb2546cb780d551bcbc6a7fad29
EunsuSeo01 <eunsu0613@naver.com>
2022-08-06 23:40:39 +0900

    Feat: 채팅 탭 추가, M1 Pods 빌드 에러 해결 (arm64 추가)

---
+++ Geeksasaeng/Geeksasaeng/View/ViewController/Chat/ChattingListVC.swift
@@ -0,0 +1,27 @@
+//
+//  ChattingListVC.swift
+//  Geeksasaeng
+//
+//  Created by 서은수 on 2022/08/06.
+//
+
+import UIKit
+
+/* 채팅방 목록을 볼 수 있는 메인 채팅탭 */
+class ChattingListViewController: UIViewController {
+
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .init(hex: 0xFCFDFE)
+        
+        /* Navigation Bar Attrs */
+        self.navigationItem.title = "채팅"
+    }
+    
+    // MARK: - Functions
+    
+    
+
+}
