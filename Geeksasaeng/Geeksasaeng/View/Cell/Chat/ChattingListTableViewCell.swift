42158eb7c2a3eb7b34adf58b2ec21d9d847b6692
EunsuSeo01 <eunsu0613@naver.com>
2022-08-07 18:12:08 +0900

    Refactor: 파일 구조, 이름 통합

---
+++ Geeksasaeng/Geeksasaeng/View/Cell/Chat/ChattingListTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChattingListTableViewCell.swift
+//  Geeksasaeng
+//
+//  Created by 서은수 on 2022/08/07.
+//
+
+import UIKit
+
+class ChattingListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
