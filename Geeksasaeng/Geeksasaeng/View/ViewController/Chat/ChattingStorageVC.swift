f7b1912c00ba20ae4f0a5adf03eff925f1575442
EunsuSeo01 <eunsu0613@naver.com>
2022-09-29 06:07:56 +0900

    GEEKSA-509 Feat: 채팅 보관함 배달파티, 심부름, 거래 필터 부분 구현

--- Geeksasaeng/Geeksasaeng/View/ViewController/Chat/ChattingStorageVC.swift
+++ Geeksasaeng/Geeksasaeng/View/ViewController/Chat/ChattingStorageVC.swift
@@ -12,6 +12,51 @@ import SnapKit
 /* 채팅 보관함 메인 화면 */
 class ChattingStorageViewController: UIViewController {
 
+    // MARK: - Properties
+    
+    // 현재 선택되어 있는 필터의 label
+    var selectedLabel: UILabel? = nil {
+        didSet {
+            if selectedLabel?.text != "배달파티" {
+                showReadyView()
+            }
+        }
+    }
+    
+    // MARK: - SubViews
+    
+    /* Filter Icon */
+    let filterImageView = UIImageView(image: UIImage(named: "FilterImage")).then {
+        $0.tintColor = UIColor(hex: 0x2F2F2F)
+    }
+    
+    /* Category Filter */
+    /* 필터뷰들을 묶어놓는 스택뷰 */
+    let filterStackView = UIStackView().then {
+        $0.spacing = 9
+        $0.axis = .horizontal
+        $0.alignment = .top
+        $0.distribution = .equalSpacing
+    }
+    
+    /* 테이블뷰 셀 하단에 블러뷰 */
+    let blurView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 140)).then {
+        // 그라데이션 적용
+        $0.setGradient(startColor: .init(hex: 0xFFFFFF, alpha: 0.0), endColor: .init(hex: 0xFFFFFF, alpha: 1.0))
+        $0.isUserInteractionEnabled = false // 블러뷰에 가려진 테이블뷰 셀이 선택 가능하도록 하기 위해
+    }
+    
+    // 준비 중입니다 화면
+    let readyView = UIView().then {
+        $0.backgroundColor = .white
+        let imageView = UIImageView(image: UIImage(named: "ReadyImage"))
+        $0.addSubview(imageView)
+        imageView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.width.equalTo(153)
+            make.height.equalTo(143)
+        }
+    }
     
     // MARK: - Life Cycle
     
@@ -19,6 +64,12 @@ class ChattingStorageViewController: UIViewController {
         super.viewDidLoad()
 
         setAttributes()
+        setFilterStackView(filterNameArray: ["배달파티", "심부름", "거래"],
+                           width: [80, 67, 54],
+                           stackView: filterStackView)
+        addSubViews()
+        setLayouts()
+        setLabelTap()
     }
     
     // MARK: - Functions
@@ -40,6 +91,101 @@ class ChattingStorageViewController: UIViewController {
         view.backgroundColor = .white
     }
     
+    private func addSubViews() {
+        [
+            filterImageView, filterStackView,
+            blurView
+        ].forEach { view.addSubview($0) }
+    }
+    
+    private func setLayouts() {
+        /* Category Filter */
+        filterImageView.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(23)
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(28)
+            make.width.equalTo(23)
+            make.height.equalTo(15)
+        }
+        filterStackView.snp.makeConstraints { make in
+            make.left.equalTo(filterImageView.snp.right).offset(17)
+            make.centerY.equalTo(filterImageView)
+            make.width.equalTo(134 + 85)
+        }
+        
+        /* Blur View */
+        blurView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(140)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
+    /* 배열에 담긴 이름으로 Filter Views를 만들고 스택뷰로 묶는다 */
+    private func setFilterStackView(filterNameArray: [String], width: [CGFloat], stackView: UIStackView) {
+        for i in 0..<filterNameArray.count {
+            let filterText = filterNameArray[i]
+            let filterView: UIView = {
+                let view = UIView()
+                view.backgroundColor = .init(hex: 0xF8F8F8)
+                view.layer.cornerRadius = 5
+                view.snp.makeConstraints { make in
+                    make.width.equalTo(width[i])
+                    make.height.equalTo(35)
+                }
+                
+                let label = UILabel()
+                label.text = filterText
+                label.font = .customFont(.neoMedium, size: 14)
+                if filterText == "배달파티" {
+                    // default로 배달파티 필터가 활성화 되어 있도록 설정
+                    label.textColor = .mainColor
+                    selectedLabel = label
+                } else {
+                    label.textColor = UIColor(hex: 0xD8D8D8)
+                }
+                view.addSubview(label)
+                label.snp.makeConstraints { make in
+                    make.centerX.centerY.equalToSuperview()
+                }
+                
+                return view
+            }()
+            
+            /* Stack View */
+            stackView.addArrangedSubview(filterView)
+        }
+    }
+    
+    /* label에 탭 제스쳐를 추가 */
+    private func setLabelTap() {
+        // filterStackView의 서브뷰 안의 label에게 탭 제스쳐를 추가한다.
+        filterStackView.subviews.forEach { view in
+            view.subviews.forEach { sub in
+                let label = sub as! UILabel
+                print(label.text!)
+                // 탭 제스쳐를 label에 추가 -> label을 탭했을 때 액션이 발생하도록.
+                let labelTapGesture = UITapGestureRecognizer(target: self,
+                                                             action: #selector(tapFilterLabel(sender:)))
+                label.isUserInteractionEnabled = true
+                label.addGestureRecognizer(labelTapGesture)
+            }
+        }
+    }
+    
+    /* 준비 중입니다 화면 띄우기 */
+    private func showReadyView() {
+        view.addSubview(readyView)
+        readyView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+            make.top.equalTo(filterStackView.snp.bottom).offset(8)
+        }
+    }
+    
+    private func removeReadyView() {
+        readyView.removeFromSuperview()
+    }
+    
     // MARK: - @objc Functions
     
     @objc
@@ -50,6 +196,28 @@ class ChattingStorageViewController: UIViewController {
     @objc
     private func tapStorageEdit() {
         print("DEBUG: 채팅 보관함 수정 버튼 클릭")
+    }
+    
+    /* 필터를 탭하면 텍스트 색깔이 바뀌도록 */
+    @objc
+    private func tapFilterLabel(sender: UITapGestureRecognizer) {
+        let label = sender.view as! UILabel
+        
+        // label 색 변경
+        // 선택돼있던 label이 아니었다면, mainColor로 바꿔준다
+        if label.textColor != .mainColor {
+            // 원래 선택돼있던 label 색깔은 해제한다. -> 필터 1개만 선택 가능
+            if selectedLabel != nil {
+                // 색깔 원상복귀
+                selectedLabel?.textColor = .init(hex: 0xD8D8D8)
+                // 이전에 선택돼있던 게 심부름/거래 였으면 준비 중입니다 뷰 삭제
+                if selectedLabel?.text != "배달파티" {
+                    removeReadyView()
+                }
+            }
+            label.textColor = .mainColor
+            selectedLabel = label
+        }
     }
 
 }
