92c23d692297ace1362895218c38a2f90c753a89
EunSuSeo01 <eunsu0613@naver.com>
2022-12-28 22:56:06 +0900

    GEEKSA-83 Feat: Realm을 사용해 채팅 데이터를 로컬에 저장하고 불러오기

--- Geeksasaeng/Geeksasaeng/Model/Chat/MessageData.swift
+++ Geeksasaeng/Geeksasaeng/Model/Chat/MessageData.swift
@@ -6,32 +6,56 @@
 //
 
 import Foundation
+import RealmSwift
 
 // 채팅 전송하는 데이터 모델
 struct MsgRequest: Encodable {
-    var content: String? = nil
-    var chatRoomId: String? = nil
-    var isSystemMessage: Bool? = nil
-    var memberId: Int? = nil
-    var email: String? = nil
-    var profileImgUrl: String? = nil
-    var chatType: String? = nil
-    var chatId: String? = nil
-    var isImageMessage: Bool = false
+    var content: String?
+    var chatRoomId: String?
+    var isSystemMessage: Bool?
+    var chatType: String?
+    var chatId: String?
+    var jwt: String?
 }
 
 // 채팅 수신하는 데이터 모델
-struct MsgResponse: Decodable {
-    var chatId: Int? = nil
-    var content: String? = nil
-    var chatRoomId: String? = nil
-    var isSystemMessage: Bool? = nil
-    var memberId: Int? = nil
-    var email: String? = nil
-    var profileImgUrl: String? = nil
-    var readMembers: [String]? = nil
-    var createdAt: String? = nil
-    var chatType: String? = nil
-    var unreadMemberCnt: Int? = nil
-    var isImageMessage: Bool? = nil
+// + Realm으로 로컬에 저장되는 채팅 데이터 모델
+class MsgResponse: Object, Decodable {
+    @Persisted(primaryKey: true) var chatId: String? = nil
+    @Persisted var content: String? = nil
+    @Persisted var chatRoomId: String? = nil
+    @Persisted var isSystemMessage: Bool? = nil
+    @Persisted var memberId: Int? = nil
+    @Persisted var profileImgUrl: String? = nil
+    @Persisted var readMembers = List<Int>()
+    @Persisted var createdAt: String? = nil
+    @Persisted var chatType: String? = nil
+    @Persisted var unreadMemberCnt: Int? = nil
+    @Persisted var isImageMessage: Bool? = nil
+    
+    // 생성자
+    convenience init(chatId: String,
+                     content: String,
+                     chatRoomId: String,
+                     isSystemMessage: Bool,
+                     memberId: Int,
+                     profileImgUrl: String,
+                     readMembers: List<Int>,
+                     createdAt: String,
+                     chatType: String,
+                     unreadMemberCnt: Int,
+                     isImageMessage: Bool) {
+        self.init()
+        self.chatId = chatId
+        self.content = content
+        self.chatRoomId = chatRoomId
+        self.isSystemMessage = isSystemMessage
+        self.memberId = memberId
+        self.profileImgUrl = profileImgUrl
+        self.readMembers = readMembers
+        self.createdAt = createdAt
+        self.chatType = chatType
+        self.unreadMemberCnt = unreadMemberCnt
+        self.isImageMessage = isImageMessage
+    }
 }
