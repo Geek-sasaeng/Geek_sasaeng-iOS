8e102173056d37c7b4872a7a84c2a65090c7398d
EunSuSeo01 <eunsu0613@naver.com>
2023-02-20 23:46:59 +0900

    Fix: 스플래쉬 화면의 애니메이션 뷰 레이아웃 SnapKit으로 잡기

--- Geeksasaeng/Geeksasaeng/View/ViewController/Splash/SplashPlayerVC.swift
+++ Geeksasaeng/Geeksasaeng/View/ViewController/Splash/SplashPlayerVC.swift
@@ -11,20 +11,24 @@ import Lottie
 /* 스플래쉬 애니메이션 */
 class SplashPlayerViewController: UIViewController {
     
+    // MARK: - SubViews
+    
+    // Lottie AnimationView 생성
+    let animationView = LottieAnimationView(name: "splash").then {
+        $0.contentMode = .scaleAspectFill
+    }
+    
     // MARK: - Life Cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Lottie AnimationView 생성
-        let animationView = LottieAnimationView(name: "splash").then {
-            $0.contentMode = .scaleAspectFill
+        // 레이아웃 설정
+        view.addSubview(animationView)
+        animationView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
         }
         
-        // 메인 뷰에 삽입
-        view.addSubview(animationView)
-        animationView.frame = animationView.superview!.bounds
-
         // 애니메이션 재생 (애니메이션 재생모드 미 설정시 1회)
         animationView.play { (finished) in
             // 끝나면 로그인뷰로 이동
