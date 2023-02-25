8e54945a2ac75b05e1d0d096128590c6b388db43
EunSuSeo01 <eunsu0613@naver.com>
2023-02-25 09:58:29 +0900

    Refactor: 이메일 인증번호 체크 함수 개선

--- Geeksasaeng/Geeksasaeng/ViewModel/Register/EmailAuthCheckViewModel.swift
+++ Geeksasaeng/Geeksasaeng/ViewModel/Register/EmailAuthCheckViewModel.swift
@@ -10,7 +10,7 @@ import Alamofire
 
 class EmailAuthCheckViewModel {
     // MEMO: - VM은 VC를 몰라야 함 / VC -> VM / 결합력
-    public static func requestCheckEmailAuth(_ parameter: EmailAuthCheckInput, completion: @escaping (Bool, Int) -> Void) {
+    public static func requestCheckEmailAuth(_ parameter: EmailAuthCheckInput, completion: @escaping (Bool, Int?) -> Void) {
         AF.request("https://geeksasaeng.shop/email/check", method: .post, parameters: parameter, encoder: JSONParameterEncoder.default, headers: nil)
             .validate()
             .responseDecodable(of: EmailAuthCheckModel.self) {
@@ -18,17 +18,15 @@ class EmailAuthCheckViewModel {
                 switch response.result {
                 case .success(let result):
                     if result.isSuccess! {
-                        print("DEBUG: 성공")
+                        print("DEBUG: 이메일 인증번호 확인 성공")
                         // 성공하면 휴대폰 인증 화면으로 넘어간다.
-                        guard let data = result.result?.emailId else { return }
-                        completion(true, data)
                     } else {
-                        print("DEBUG: 실패", result.message!)
-                        completion(false, 0)
+                        print("DEBUG: 이메일 인증번호 확인 실패", result)
                     }
+                    completion(result.isSuccess!, result.result?.emailId)
                 case .failure(let error):
-                    print("DEBUG:", error.localizedDescription)
-                    completion(false, 0)
+                    print("DEBUG: 이메일 인증번호 확인 실패", error.localizedDescription)
+                    completion(false, nil)
                 }
             }
         
